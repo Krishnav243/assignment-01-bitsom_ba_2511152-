b22189ee8ccce732db4785a8577696a9194375a0
Krishnav243 <krishnavbajaj89@gmail.com>
2026-03-23 18:55:05 +0530

    Update mongo_queries.js

--- part2-nosql/mongo_queries.js
+++ part2-nosql/mongo_queries.js
@@ -1,15 +1,15 @@
 // OP1
-db.products.insertMany([]);
+db.products.insertMany([...]);
 
 // OP2
 db.products.find({ category: "Electronics", price: { $gt: 20000 } });
 
 // OP3
-db.products.find({ category: "Groceries" });
+db.products.find({ category: "Groceries", expiry_date: { $lt: "2025-01-01" } });
 
 // OP4
 db.products.updateOne(
-  { name: "Laptop" },
+  { name: "iPhone 14" },
   { $set: { discount_percent: 10 } }
 );
 
