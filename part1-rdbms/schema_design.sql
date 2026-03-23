11c88f73be73eb34b82c78f28dfd0c215657a2b6
Krishnav243 <krishnavbajaj89@gmail.com>
2026-03-23 18:48:38 +0530

    Update schema_design.sql

--- part1-rdbms/schema_design.sql
+++ part1-rdbms/schema_design.sql
@@ -1,1 +1,32 @@
+CREATE TABLE customers (
+    customer_id VARCHAR(10) PRIMARY KEY,
+    customer_name VARCHAR(100),
+    city VARCHAR(50)
+);
 
+CREATE TABLE products (
+    product_id VARCHAR(10) PRIMARY KEY,
+    product_name VARCHAR(100),
+    price INT
+);
+
+CREATE TABLE sales_reps (
+    rep_id VARCHAR(10) PRIMARY KEY,
+    rep_name VARCHAR(100)
+);
+
+CREATE TABLE orders (
+    order_id VARCHAR(10) PRIMARY KEY,
+    customer_id VARCHAR(10),
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+CREATE TABLE order_items (
+    order_id VARCHAR(10),
+    product_id VARCHAR(10),
+    quantity INT,
+    PRIMARY KEY (order_id, product_id),
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
